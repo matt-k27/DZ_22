9bfe324dd966880e5a323460b5277fc1d2083e3a
Matt <matt@MacBook-Pro-Matt.local>
2024-03-24 21:55:04 +0100

    *tup

---
+++ main.py
@@ -0,0 +1,17 @@
+print('DZ22')
+
+
+def difference(*tup):
+    if not tup:
+        return 0
+    else:
+        return round(max(tup) - min(tup), 2)
+
+
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
+print('OK')
+
+print('Thank you for using')
